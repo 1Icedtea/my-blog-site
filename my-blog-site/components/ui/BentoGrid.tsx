777cb43c52b9752f85d82cf901f003b6ecda1b3a
IcedTea <fistfireace37@gmail.com>
2024-05-21 04:14:06 +0800

    more styling, added tech stack and globe elements

--- my-blog-site/components/ui/BentoGrid.tsx
+++ my-blog-site/components/ui/BentoGrid.tsx
@@ -1,5 +1,7 @@
 import { cn } from "@/utils/cn";
 import { BackgroundGradientAnimation } from "./GradientBG";
+import { Globe } from "./Globe";
+import { GlobeDemo } from "./GridGlobe";
 
 export const BentoGrid = ({
   className,
@@ -44,7 +46,7 @@ export const BentoGridItem = ({
   return (
     <div
       className={cn(
-        "row-span-1 rounded-3xl relative hover:shadow-xl transition duration-200 shadow-input dark:shadow-none p-4 dark:bg-black dark:border-white/[0.2] bg-white border border-transparent justify-between flex flex-col space-y-4",
+        "row-span-1 rounded-3xl relative overflow-hidden hover:shadow-xl transition duration-200 shadow-input dark:shadow-none p-4 dark:bg-black dark:border-white/[0.2] bg-white border border-transparent justify-between flex flex-col space-y-4",
         className
       )}
       style={{
@@ -93,6 +95,21 @@ export const BentoGridItem = ({
               {title}
             </div>
           </div>
+          {id === 2 && <GlobeDemo />}
+          {id === 3 && (
+            <div className="flex gap-1 lg:gap-5 w-fit absolute -right-3 lg:-right-2">
+              <div className="flex flex-col gap-3 lg:gap-8">
+                {["Next.js", "TypeScript", "Tailwind"].map((item) => (
+                  <span
+                    key={item}
+                    className="py-3 lg:py-4 px-3 text-xs lg:text-base opacity-50 lg:opacity-100 rounded-lg text-center bg-[#10132e]"
+                  >
+                    {item}
+                  </span>
+                ))}
+              </div>
+            </div>
+          )}
         </div>
       </div>
       <div className="group-hover/bento:translate-x-2 transition duration-200"></div>
