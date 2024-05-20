bc28288cadd506b57026dbab2715a98dda07ac95
IcedTea <fistfireace37@gmail.com>
2024-05-21 03:55:25 +0800

    more styling and minor styling bug fixes

--- my-blog-site/components/ui/BentoGrid.tsx
+++ my-blog-site/components/ui/BentoGrid.tsx
@@ -1,4 +1,5 @@
 import { cn } from "@/utils/cn";
+import { BackgroundGradientAnimation } from "./GradientBG";
 
 export const BentoGrid = ({
   className,
@@ -10,7 +11,7 @@ export const BentoGrid = ({
   return (
     <div
       className={cn(
-        "grid md:auto-rows-[18rem] grid-cols-1 md:grid-cols-3 gap-4 max-w-7xl mx-auto ",
+        "grid group/bento grid-cols-1 md:grid-cols-6 lg:grid-cols-5 gap-4 lg:gap-8 mx-auto ",
         className
       )}
     >
@@ -43,7 +44,7 @@ export const BentoGridItem = ({
   return (
     <div
       className={cn(
-        "row-span-1 rounded-3xl relative group/bento hover:shadow-xl transition duration-200 shadow-input dark:shadow-none p-4 dark:bg-black dark:border-white/[0.2] bg-white border border-transparent justify-between flex flex-col space-y-4",
+        "row-span-1 rounded-3xl relative hover:shadow-xl transition duration-200 shadow-input dark:shadow-none p-4 dark:bg-black dark:border-white/[0.2] bg-white border border-transparent justify-between flex flex-col space-y-4",
         className
       )}
       style={{
@@ -71,19 +72,30 @@ export const BentoGridItem = ({
             <img
               src={missImg}
               alt={missImg}
-              className={`object-cover w-full h-full object-center`}
+              className={`object-cover, object-center w-full h-full`}
             />
           )}
         </div>
-      </div>
-      <div className="group-hover/bento:translate-x-2 transition duration-200">
-        <div className="font-sans font-bold text-neutral-600 dark:text-neutral-200 mb-2 mt-2">
-          {title}
+        {id === 6 && (
+          <BackgroundGradientAnimation>
+            <div className="absolute z-50 flex items-center justify-center text-white font-bold" />
+          </BackgroundGradientAnimation>
+        )}
+        <div
+          className={cn(
+            titleClassName,
+            "group-hover/bento: translate-x-2 transition duration-200 relative md:h-full min-h-40 flex flex-col px-5 p-5 lg:p-10"
+          )}
+        >
+          <div className="font-sans font-extralight text-neutral-300 text-sm md:text-xs lg:text-base z-10">
+            {description}
+            <div className="font-sans font-bold text-lg lg:text-3xl max-w-96 z-10">
+              {title}
+            </div>
+          </div>
         </div>
-        <div className="font-sans font-normal text-neutral-600 text-xs dark:text-neutral-300">
-          {description}
-        </div>
       </div>
+      <div className="group-hover/bento:translate-x-2 transition duration-200"></div>
     </div>
   );
 };
