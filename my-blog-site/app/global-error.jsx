540f991149eb20ec2ca482b5954486b3fb1c470d
IcedTea <fistfireace37@gmail.com>
2024-05-27 03:10:50 +0800

    fixed errors on sentry configuration

--- my-blog-site/app/global-error.jsx
+++ my-blog-site/app/global-error.jsx
@@ -1,20 +1,19 @@
+"use client";
+
 import * as Sentry from "@sentry/nextjs";
 import Error from "next/error";
-import { withSentryConfig } from "@sentry/nextjs";
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+import { useEffect } from "react";
 
 export default function GlobalError({ error }) {
   useEffect(() => {
     Sentry.captureException(error);
-
-    return () => {
-      second;
-    };
   }, [error]);
+
   return (
     <html>
-      <body>{GlobalError}</body>
+      <body>
+        <Error />
+      </body>
     </html>
   );
 }
